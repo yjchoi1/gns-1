b364a64107217190c45b16daeacba12249215c60
baagee <yjchoi9212@gmail.com>
2023-08-20 19:31:44 -0500

    fix cuda and cpu check

--- gns/train.py
+++ gns/train.py
@@ -300,7 +300,7 @@ def train(rank, flags, world_size):
           print(f'Training step: {step}/{flags["ntraining_steps"]}. Loss: {loss}.')
           # Save model state
           if step % flags["nsave_steps"] == 0:
-            if torch.device("cpu"):
+            if rank == torch.device("cpu"):
               simulator.save(flags["model_path"] + 'model-'+str(step)+'.pt')
             else:
               simulator.module.save(flags["model_path"] + 'model-'+str(step)+'.pt')
@@ -320,7 +320,7 @@ def train(rank, flags, world_size):
     pass
 
   if rank == 0 or torch.device("cpu"):
-    if torch.device("cpu"):
+    if rank == torch.device("cpu"):
       simulator.save(flags["model_path"] + 'model-'+str(step)+'.pt')
     else:
       simulator.module.save(flags["model_path"] + 'model-'+str(step)+'.pt')
